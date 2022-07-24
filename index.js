a4ac102495cb1c142a132c017b90879f10d234df
OliverPrada <49795876+OliverPradaLara@users.noreply.github.com>
2022-07-24 19:49:42 -0400

    update index remoto

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Palindrome = (myString) => {
     return myString.split('').reverse().join('') === myString
 }
+//pa probar un cambio
 
 console.log('resultado:',Palindrome("anitalavalatina"))
