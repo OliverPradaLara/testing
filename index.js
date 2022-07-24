e62e23b0263fa44ea04ce2798b3504a46656ed69
OliverPrada <49795876+OliverPradaLara@users.noreply.github.com>
2022-07-24 19:52:09 -0400

    update para generar conflicto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Palindrome = (myString) => {
     return myString.split('').reverse().join('') === myString
 }
-//pa probar un cambio
+//pa probar un poliedro
 
 console.log('resultado:',Palindrome("anitalavalatina"))
