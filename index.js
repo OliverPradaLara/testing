c8f31dc26215f2a6ce933a4ad343fdb5d2e5c9cd
OliverPradaLara <oprada.java.developer@gmail.com>
2022-07-24 19:53:13 -0400

    cambios para generar conflicto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Palindrome = (myString) => {
     return myString.split('').reverse().join('') === myString
 }
-//pa probar un cambio
+//pa probar un luis paricio
 
 console.log('resultado:',Palindrome("anitalavalatina"))
